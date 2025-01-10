1c0063355d7de1e980634f4d35a2ba926cf7abc1
24LP3431 24LP3431 <24LP3431@G302-CC0115.local>
2025-01-10 19:29:37 +0800

    finished all stylestwo

--- src/app/_components/darkmode.tsx
+++ src/app/_components/darkmode.tsx
@@ -1,17 +1,2 @@
 "use client";
 import { useState } from "react";
-
-export function DarkMode({ isClicked }) {
-  const setDarkMode = () => {
-    document.querySelector("body")?.setAttribute("data-theme", "dark");
-  };
-
-  const setWhiteMode = () => {
-    document.querySelector("body")?.setAttribute("data-theme", "white");
-  };
-  if (isClicked === true) {
-    setDarkMode;
-  } else setWhiteMode;
-  DarkMode({ isClicked });
-  return <></>;
-}
